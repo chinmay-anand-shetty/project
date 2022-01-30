0b0940381623948dfa19f7a362ef1f351731c7ed
chinmay-anand-shetty <97421045+chinmay-anand-shetty@users.noreply.github.com>
2022-01-30 18:15:11 +0530

    Update mysql.tf

--- terraform/mysql.tf
+++ terraform/mysql.tf
@@ -10,21 +10,21 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
-resource "aws_db_instance" "samuraihunter" {
+resource "aws_db_instance" "demo_user" {
   allocated_storage       = 08             
   engine                  = "mysql"
   engine_version          = "5.7"
   instance_class          = "db.t2.micro"
-  identifier              = "samurai"
-  name                    = "samurai"
-  username                = "samurai"
-  password                = "samuraihunter"
+  identifier              = "identifying_name"
+  name                    = "user"
+  username                = "user_name"
+  password                = "*******"
   multi_az                = "false"
   storage_type            = "gp2"
   backup_retention_period = 30
   skip_final_snapshot     = true
   publicly_accessible     = true
   tags = {
-    Name = "samuraihunter_instance"
+    Name = "name_instance"
   }
 }
